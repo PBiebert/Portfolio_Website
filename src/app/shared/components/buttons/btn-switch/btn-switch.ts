14ce8b9076e14c7394e5698624723b225bdaf9bd
Philipp Biebert <philipp.biebert@gmail.com>
2025-12-19 12:19:29 +0100

    feat: add the header: - embedding logo - create the navbar - create a switch button

---
+++ src/app/shared/components/buttons/btn-switch/btn-switch.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-switch',
+  imports: [],
+  templateUrl: './btn-switch.html',
+  styleUrl: './btn-switch.scss',
+})
+export class BtnSwitch {
+
+}
---
+++ src/app/shared/components/header/nav/nav.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { BtnSwitch } from '../../buttons/btn-switch/btn-switch';
+
+@Component({
+  selector: 'app-nav',
+  imports: [BtnSwitch],
+  templateUrl: './nav.html',
+  styleUrl: './nav.scss',
+})
+export class Nav {}

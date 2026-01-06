5f67240a9a3ecceff9b0d05470147a787503fe2b
Philipp Biebert <philipp.biebert@gmail.com>
2026-01-06 15:24:06 +0100

    feat: add basic button styles; create social-links component; hero-area: add buttons and include social-links; refactor styles

--- src/app/pages/home/sections/hero/hero.ts
+++ src/app/pages/home/sections/hero/hero.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { SocialLinks } from '../../../../shared/components/social-links/social-links';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [SocialLinks],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
-export class Hero {
-
-}
+export class Hero {}

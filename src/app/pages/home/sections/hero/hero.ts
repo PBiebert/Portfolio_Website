41169286aa71db8b595af57a4500a770f6166a04
Philipp Biebert <philipp.biebert@gmail.com>
2026-01-13 11:18:57 +0100

    refactor: move polaroid and hello-world button into hero section

--- src/app/pages/home/sections/hero/hero.ts
+++ src/app/pages/home/sections/hero/hero.ts
@@ -2,16 +2,43 @@ import { Component } from '@angular/core';
 import { SocialLinks } from '../../../../shared/components/social-links/social-links';
 import { InvertCase } from '../../../../shared/directives/invert-case';
 import { CommonModule } from '@angular/common';
-import { Polaroid } from './components/polaroid/polaroid';
-import { BtnHelloWorld } from './components/btn-hello-world/btn-hello-world';
 
 @Component({
   selector: 'app-hero',
-  imports: [SocialLinks, InvertCase, CommonModule, Polaroid, BtnHelloWorld],
+  imports: [SocialLinks, InvertCase, CommonModule],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
 export class Hero {
   contentFirstLine = 'Fullstack'.split('');
   contentSecondLine = 'DEVELOPER'.split('');
+
+  HelloWorldBtnHover: boolean = false;
+  helloWorldBtnContent = 'Hello World';
+
+  hoverAnimationRunning: boolean = false;
+  isOnPolaroid: boolean = false;
+
+  isOnHelloWorldBtn() {
+    this.helloWorldBtnContent = 'Mein Name ist Philipp';
+  }
+
+  isNotOnHelloWorldBtn() {
+    this.helloWorldBtnContent = 'Hello World';
+  }
+
+  isOnPicture() {
+    this.isOnPolaroid = true;
+    this.startFrameHoverAnimation();
+  }
+
+  isNotOnPicture() {
+    this.isOnPolaroid = false;
+    this.startFrameHoverAnimation();
+  }
+
+  startFrameHoverAnimation() {
+    setTimeout(() => (this.hoverAnimationRunning = false), 500);
+    this.hoverAnimationRunning = true;
+  }
 }

90018de7c08bc06074537332581c26684ebbe635
Philipp Biebert <philipp.biebert@gmail.com>
2026-01-13 16:42:08 +0100

    feat: add the stack-section

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from './sections/hero/hero';
 import { AboutMe } from './sections/about-me/about-me';
+import { Stack } from './sections/stack/stack';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, AboutMe],
+  imports: [Hero, AboutMe, Stack],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })

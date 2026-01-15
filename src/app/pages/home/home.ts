4d9c93fa80fa29a09137a052233d660fe1a964eb
Philipp Biebert <philipp.biebert@gmail.com>
2026-01-15 14:25:05 +0100

    feat: add contact-me section

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,10 +4,11 @@ import { AboutMe } from './sections/about-me/about-me';
 import { Stack } from './sections/stack/stack';
 import { Projects } from './sections/projects/projects';
 import { References } from './sections/references/references';
+import { ContactMe } from './sections/contact-me/contact-me';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, AboutMe, Stack, Projects, References],
+  imports: [Hero, AboutMe, Stack, Projects, References, ContactMe],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })

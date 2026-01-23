571ce0a1c6abe6236b7fcd07be9192ae5f58ded9
Philipp Biebert <philipp.biebert@gmail.com>
2026-01-23 09:13:27 +0100

    feat(contact): style contact form and implement submit logic

--- src/app/pages/home/sections/contact-me/contact-me.ts
+++ src/app/pages/home/sections/contact-me/contact-me.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ImgReveal } from '../../../../shared/directives/img-reveal';
 import { Footer } from '../../../../shared/components/footer/footer';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [ImgReveal, Footer],
+  imports: [ImgReveal, Footer, FormsModule],
   templateUrl: './contact-me.html',
   styleUrl: './contact-me.scss',
 })
-export class ContactMe {}
+export class ContactMe {
+  http = inject(HttpClient);
+
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+    privacyCheck: false,
+  };
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData)).subscribe({
+        next: (response) => {
+          ngForm.resetForm();
+        },
+        error: (error) => {
+          console.error(error);
+        },
+        complete: () => console.info('send post complete'),
+      });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      ngForm.resetForm();
+    }
+  }
+}

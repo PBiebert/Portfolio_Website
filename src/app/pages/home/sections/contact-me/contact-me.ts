e501109f1f4ba476f4021786ffce4d6a6b9cd4f0
Philipp Biebert <philipp.biebert@gmail.com>
2026-01-24 10:40:12 +0100

    feat: add dialog to contact section after message is sent

--- src/app/pages/home/sections/contact-me/contact-me.ts
+++ src/app/pages/home/sections/contact-me/contact-me.ts
@@ -1,10 +1,10 @@
-import { Component, ElementRef, inject, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, Renderer2, ViewChild } from '@angular/core';
 import { ImgReveal } from '../../../../shared/directives/img-reveal';
 import { Footer } from '../../../../shared/components/footer/footer';
 import { FormsModule, NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { RouterLink } from '@angular/router';
-
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-contact-me',
   imports: [ImgReveal, Footer, FormsModule, RouterLink],
@@ -12,7 +12,9 @@ import { RouterLink } from '@angular/router';
   styleUrl: './contact-me.scss',
 })
 export class ContactMe {
-  http = inject(HttpClient);
+  private renderer = inject(Renderer2);
+  private document = inject(DOCUMENT);
+  private http = inject(HttpClient);
   @ViewChild('thankYouDialog') thankYouDialog!: ElementRef<HTMLDialogElement>;
 
   contactData = {
@@ -24,7 +26,7 @@ export class ContactMe {
 
   mailTest = true;
 
-  post = {
+  private post = {
     endPoint: 'https://deineDomain.de/sendMail.php',
     body: (payload: any) => JSON.stringify(payload),
     options: {
@@ -39,6 +41,8 @@ export class ContactMe {
     if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
       this.http.post(this.post.endPoint, this.post.body(this.contactData)).subscribe({
         next: (response) => {
+          this.openThankYouDialog();
+          this.autoCloseThankYouDialog();
           ngForm.resetForm();
         },
         error: (error) => {
@@ -47,8 +51,24 @@ export class ContactMe {
         complete: () => console.info('send post complete'),
       });
     } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-      this.thankYouDialog.nativeElement.showModal();
+      this.openThankYouDialog();
+      this.autoCloseThankYouDialog();
+
       ngForm.resetForm();
     }
+  }
+
+  private openThankYouDialog() {
+    this.thankYouDialog.nativeElement.showModal();
+    this.renderer.addClass(this.document.body, 'no-scroll-y');
+  }
+
+  closeThankYouDialog() {
+    this.thankYouDialog.nativeElement.close();
+    this.renderer.removeClass(this.document.body, 'no-scroll-y');
+  }
+
+  private autoCloseThankYouDialog() {
+    setTimeout(() => this.closeThankYouDialog(), 5000);
   }
 }

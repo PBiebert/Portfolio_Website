b8f9cca92dec59a3a3a0ccfdb62a2ef7f02b8506
Philipp Biebert <philipp.biebert@gmail.com>
2026-01-24 15:09:03 +0100

    style: adjust global styles and add project-in-progress page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { ProjectDetails } from './pages/project/project-details/project-details';
 import { Imprint } from './pages/imprint/imprint';
+import { Privacy } from './pages/privacy/privacy';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'single-project/:id', component: ProjectDetails },
   { path: 'imprint', component: Imprint },
-  { path: 'privacy', component: Imprint },
+  { path: 'privacy', component: Privacy },
 ];

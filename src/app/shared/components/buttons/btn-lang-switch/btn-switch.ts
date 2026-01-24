7fb660215629b1ebac5aae3711ee819c744bdf73
Philipp Biebert <philipp.biebert@gmail.com>
2026-01-24 13:41:42 +0100

    feat(lang-toggle): make selection clickable and add animation

--- src/app/shared/components/buttons/btn-lang-switch/btn-switch.ts
+++ src/app/shared/components/buttons/btn-lang-switch/btn-switch.ts
@@ -1,9 +1,34 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-btn-lang-switch',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './btn-switch.html',
   styleUrl: './btn-switch.scss',
 })
-export class BtnLangSwitch {}
+export class BtnLangSwitch {
+  overlayImg: string = 'assets/icons/lang-toggle-frame.png';
+  timerExpired: boolean = true;
+  lang: 'de' | 'en' = 'de';
+  activeOverlay: 'de' | 'en' | null = null;
+
+  setLang(lang: 'de' | 'en') {
+    this.lang = lang;
+    this.showOverlay(lang);
+  }
+
+  toggleLang() {
+    this.lang = this.lang === 'de' ? 'en' : 'de';
+    this.showOverlay(this.lang);
+  }
+
+  showOverlay(lang: 'de' | 'en') {
+    this.activeOverlay = lang;
+    this.timerExpired = false;
+    setTimeout(() => {
+      this.timerExpired = true;
+      this.activeOverlay = null;
+    }, 1000);
+  }
+}

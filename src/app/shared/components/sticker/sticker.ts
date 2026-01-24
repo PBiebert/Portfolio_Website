1c02a7b39ed319aa91af39f722dd0f8b6fe8c352
Philipp Biebert <philipp.biebert@gmail.com>
2026-01-24 17:23:10 +0100

    feat: create the component sticker

--- src/app/shared/components/sticker/sticker.ts
+++ src/app/shared/components/sticker/sticker.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sticker',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './sticker.scss',
 })
 export class Sticker {
-
+  @Input() bgColor: 'orange' | 'yellow' = 'orange';
 }
